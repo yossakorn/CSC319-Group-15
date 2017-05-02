82ace2d569be405f66f1038734937fc872cefaad
mabotan <mmymoddum@gmail.com>
2017-05-02 12:44:57 +0700

    Complete

--- NiftyEvil/src/niftyevil/Hangman.java
+++ NiftyEvil/src/niftyevil/Hangman.java
@@ -31,7 +31,7 @@ public class Hangman {
         keys = new TreeSet<String>(); //empty set
         words = new TreeSet<String>(); //
         cluster = new TreeMap<String,TreeSet<String>>();//cluster 
-        f = new File("C:\\Users\\User\\Desktop\\hangman\\dictionary.txt");
+        f = new File("C:\\Users\\Window10\\Desktop\\NiftyEvil\\dictionary.txt");
         input = new Scanner(f);
         while(input.hasNext()){ //loop import value from dic to word
             String wordtemp = input.nextLine();
@@ -54,4 +54,120 @@ public class Hangman {
         }
         System.out.println("");
     }
+    
+    public void cluster(String letter){
+        myLetter = letter;
+        generateKey();
+        for(String key : keys){
+            cluster.put(key, new TreeSet<String>());
+        }
+        for(String word : words){
+            if(word.length() != wordLength) continue;
+            for(String key : keys){
+                if(compareWord(key,word)){
+                    cluster.get(key).add(word);
+                }
+            }
+        }
+    }
+    
+    public void showCluster(){
+        if(cluster == null) return;
+        for(String key :keys){
+            System.out.print(key + " : ");
+            for(String word : cluster.get(key)){
+                System.out.print(word + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public void updateNewGroup(){
+        int max = Integer.MIN_VALUE;
+        String maxkey = ""; 
+        for(String key : keys){
+            int n = cluster.get(key).size();
+            if(n > max){
+                max = n;
+                maxkey = key;
+            }
+        }
+        updateWordProgess(maxkey);
+        words = cluster.get(maxkey);
+        myLetter = null;
+        cluster.clear();
+    }
+    
+    public void printWordProgress(){
+        System.out.println(wordprogress);
+    }
+    
+    public boolean isCorrect(){
+        return checkWord();
+    }
+    
+    private boolean checkWord(){
+        boolean correct = true;
+       for(int i = 0 ; i < wordLength ; i++){
+           if(wordprogress.charAt(i) == '-'){
+               correct = false;
+               break;
+           }
+       }
+       return correct;
+    }
+    
+    private void updateWordProgess(String key){
+        ArrayList<Integer> indexs = new ArrayList<Integer>();
+        for(int i = 0 ; i < key.length() ; i++){
+           if(key.charAt(i) == myLetter.charAt(0))indexs.add(i); 
+        }
+         for(int index : indexs){
+               wordprogress = wordprogress.substring(0,index) + myLetter + wordprogress.substring(index+1);
+         }
+         
+    }
+    
+    private boolean compareWord(String key, String word){
+        boolean correct = true;
+        ArrayList<Integer> indexs = new ArrayList<Integer>();
+        ArrayList<Integer> wordIndexs = new ArrayList<Integer>();
+        for(int i = 0 ; i < key.length() ; i++){
+            if(key.charAt(i) == myLetter.charAt(0))indexs.add(i);
+             if(word.charAt(i) == myLetter.charAt(0))indexs.add(i);    
+        }
+        if(indexs.size() == 0 && wordIndexs.size() > 0) correct = false;
+        if(correct == true){
+            for(int index : indexs){
+                if(key.charAt(index) != word.charAt(index)){
+                    correct = false;
+                    break;
+                }
+            }
+        }
+        return correct;
+    }
+    
+    private void generateKey(){
+        String key = "";
+        subset(0, wordLength, key);
+    }
+    
+    private void subset(int position, int length,String mySubset){
+        if(position == length){
+            keys.add(mySubset);
+            return;
+        }
+        
+        mySubset = mySubset + "-";
+        subset(position+1, length, mySubset);
+        mySubset = mySubset.substring(0, mySubset.length()-1);
+        mySubset = mySubset + myLetter;
+        subset(position+1, length, mySubset);
+        
+    }
+
 }
+
+    
+
--- NiftyEvil/src/niftyevil/NiftyEvil.java
+++ NiftyEvil/src/niftyevil/NiftyEvil.java
@@ -3,9 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package niftyevil;
+package NiftyEvil;
 
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  *
@@ -16,9 +17,42 @@ public class NiftyEvil {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws FileNotFoundException {
-       Hangman h = new Hangman(4);
-       h.showData();
+    public static void main(String[]args) throws FileNotFoundException{
+        Scanner input = new Scanner(System.in);
+        int play = 1;
+        do{
+            System.out.print("Please input length of word : ");
+            int numberOfWord = Integer.parseInt(input.nextLine());
+            Hangman h = new Hangman(numberOfWord);
+            if(!h.haveWords())continue;
+            System.out.print("Please input number of guess : ");
+            int guess = Integer.parseInt(input.nextLine());
+            System.out.print("Do you want to see total remainning of words (1 = yes, 0 = no) : ");
+            int showremain = Integer.parseInt(input.nextLine());
+            for(int i = 1 ; i <= guess ; i++){
+                System.out.print("Input the letter No" + (i+1) + " : ");
+                String letter = input.nextLine();
+                h.cluster(letter);
+                h.updateNewGroup();
+                h.printWordProgress();
+                if(h.isCorrect())break;
+                if(showremain == 1) h.showData();
+            }
+            if(h.isCorrect()){
+                System.out.println("You are correct !!!");
+            }
+            else System.out.println("You are worng!!");
+            System.out.println("Do you want to play again ? (1 = yes, 0 = no) : ");
+            play = Integer.parseInt(input.nextLine());
+            System.out.println("-----------------------------------------------------------------");
+        }while(play == 1);
+       
+       
+       /* Hangman n = new Hangman(4);
+        n.cluster("e");
+        n.showCluster();
+        n.updateNewGroup();
+        n.printWordProgress();*/
     }
     
 }
